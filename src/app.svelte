8759cefb90494e67cc5fcab6841d6d62b58663a4
PromiSe#### <vicimpa@ya.ru>
2023-07-22 01:36:45 +0700

    Initial commit

---
+++ src/app.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  const map = new Uint8Array(9);
+
+
+  function click(id: number) {
+    
+  }
+</script>
+
+<div class="game">
+  {#each map as val,id }
+    <div 
+      class="item" 
+      on:mousedown={() => click(id)} 
+      data-val={val} />
+  {/each}
+</div>
+
+<button class="favorite styled" type="button">
+  Restart
+</button>
